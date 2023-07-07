a328d59b3da65f867e2be764c1689339ddfd3643
razzaksr <razzaksr@gmail.com>
2023-07-07 12:58:11 +0530

    Stack OPERATION IN ARRAY DONE

---
+++ src/main/java/data/java/structures/App.java
@@ -0,0 +1,13 @@
+package data.java.structures;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
